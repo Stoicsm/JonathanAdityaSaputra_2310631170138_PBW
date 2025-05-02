9aa509a21335c5cefcdee5a0fec605a0dc4c887d
Jonathan Aditya Saputra <161813325+Stoicsm@users.noreply.github.com>
2025-05-02 11:18:36 +0700

    Pratikum.php

--- Pratikum/PBW_Pratikum6/index.php
+++ Pratikum/PBW_Pratikum6/index.php
@@ -1,27 +1,55 @@
 <?php
-// Konstanta pajak
-define("PAJAK", 0.10);
+   echo "Halo, selamat datang di dunia PHP!";
+?>
+<?php
+   $nama = "Budi";
+   $umur = 20;
 
-// Data barang dan harga
-$barang = array("Keyboard" => 150000);
 
-// Barang yang dibeli
-$nama_barang = "Keyboard";
-$jumlah_beli = 2;
+   // Menampilkan nilai variabel
+   echo "Nama: " . $nama . "<br>";
+   echo "Umur: " . $umur . " tahun<br>";
+?>
+define("NAMA_KONSTANTA", nilai);
+<?php
+define("SITE_NAME", "unsika.ac.id");
+define("VERSION", "1.0");
 
-// Proses hitung
-$harga_satuan = $barang[$nama_barang];
-$total_sebelum_pajak = $harga_satuan * $jumlah_beli;
-$pajak = $total_sebelum_pajak * PAJAK;
-$total_bayar = $total_sebelum_pajak + $pajak;
 
-// Tampilkan hasil ke layar
-echo "PERHITUNGAN TOTAL PEMBELIAN (DENGAN ARRAY)\n";
-echo "------------------------------------------\n";
-echo "Nama Barang          : $nama_barang\n";
-echo "Harga Satuan         : Rp " . number_format($harga_satuan, 0, ',', '.') . "\n";
-echo "Jumlah Beli          : $jumlah_beli\n";
-echo "Total Sebelum Pajak  : Rp " . number_format($total_sebelum_pajak, 0, ',', '.') . "\n";
-echo "Pajak (10%)          : Rp " . number_format($pajak, 0, ',', '.') . "\n";
-echo "TOTAL BAYAR          : Rp " . number_format($total_bayar, 0, ',', '.') . "\n";
+echo "Selamat datang di " . SITE_NAME . "<br>";
+echo "Versi Sistem: " . VERSION . "<br>";
+?>
+<?php
+   $nama = "Andi";
+   echo "Nama saya adalah”. $nama;
+?>
+<?php
+   $umur = 25;
+   echo "Umur saya”. $umur. “tahun";
+?>
+<?php
+   $berat = 55.5;
+   echo "Berat badan saya". $berat ."kg";
+?>
+<?php
+   $isLogin = true;
+?>
+<?php
+   $buah = ["apel", "jeruk", "mangga"];
+   echo $buah[1];
+?>
+<?php
+   class Mahasiswa {
+       public $nama;
+       public function sapa() {
+           return "Halo, saya $this->nama";
+       }
+   }
+   $mhs = new Mahasiswa();
+   $mhs->nama = "Jeni";
+   echo $mhs->sapa();
+?>
+<?php
+   $data = null;
+   var_dump($data);
 ?>
